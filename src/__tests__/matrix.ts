8a087e508ba80e7541557367afa915a9f8b226a7
Anton Gribov <antngibv@yandex.ru>
2020-01-04 01:04:49 +0300

    matrix

---
+++ src/__tests__/matrix.ts
@@ -0,0 +1,79 @@
+import Matrix from "../code/Matrix";
+
+it('New matrix has W*H items', () => {
+    const matrix = new Matrix(10, 11);
+    expect(matrix.width).toBe(10);
+    expect(matrix.height).toBe(11);
+    expect(matrix.items).toHaveLength(10*11);
+});
+
+it('Set item and get it then', () => {
+    const matrix = new Matrix<number>(3, 3);
+    expect(matrix.itemAt(1, 1)).not.toBe(5)
+    
+    matrix.setItemAt(1, 1, 5);
+    expect(matrix.itemAt(1, 1)).toBe(5);
+});
+
+it('Get out of range throws an error', () => {
+    const matrix = new Matrix<number>(3, 3);
+    expect(() => matrix.itemAt(-1, 0)).toThrowError(/^x/);
+    expect(() => matrix.itemAt(3, 0)).toThrowError(/^x/);
+    expect(() => matrix.itemAt(0, -1)).toThrowError(/^y/);
+    expect(() => matrix.itemAt(0, 3)).toThrowError(/^y/);
+})
+
+it('fill method fills as expected', () => {
+    const matrix = new Matrix<number>(3, 3);
+    
+    matrix.fill((x, y) => (x+1)*10 + y+1);
+
+    expect(matrix.itemAt(0, 0)).toBe(11);
+    expect(matrix.itemAt(0, 1)).toBe(12);
+    expect(matrix.itemAt(0, 2)).toBe(13);
+
+    expect(matrix.itemAt(1, 0)).toBe(21);
+    expect(matrix.itemAt(1, 1)).toBe(22);
+    expect(matrix.itemAt(1, 2)).toBe(23);
+
+    expect(matrix.itemAt(2, 0)).toBe(31);
+    expect(matrix.itemAt(2, 1)).toBe(32);
+    expect(matrix.itemAt(2, 2)).toBe(33);
+});
+
+it('get fields around', () => {
+    const matrix = new Matrix<number>(3, 3);
+    matrix.fill((x, y) => (x+1)*10 + y+1);
+
+    const items = matrix.getItemsAround(1, 1);
+
+    expect(items).toEqual([11,12,13,21,23,31,32,33]);
+});
+
+it('get fields around top center', () => {
+    const matrix = new Matrix<number>(3, 3);
+    matrix.fill((x, y) => (x+1)*10 + y+1);
+
+    const items = matrix.getItemsAround(0, 1);
+
+    expect(items).toEqual([11,13,21,22,23]);
+});
+
+it('get fields around top left', () => {
+    const matrix = new Matrix<number>(3, 3);
+    matrix.fill((x, y) => (x+1)*10 + y+1);
+
+    const items = matrix.getItemsAround(0, 0);
+
+    expect(items).toEqual([12,21,22]);
+});
+
+it('get fields around bottom left', () => {
+    const matrix = new Matrix<number>(3, 3);
+    matrix.fill((x, y) => (x+1)*10 + y+1);
+
+    const items = matrix.getItemsAround(2, 0);
+
+    expect(items).toEqual([21,22,32]);
+});
+
---
+++ src/code/Matrix.ts
@@ -0,0 +1,74 @@
+export default class Matrix<T> {
+    private _items: T[];
+
+    public get items(): T[] {
+        return this._items;
+    }
+
+    public get width(): number {
+        return this._width;
+    }
+
+    public get height(): number {
+        return this._height;
+    }
+
+    constructor(private _width: number, private _height: number) {
+        this._items = new Array(_width * _height)
+    }
+
+    public itemAt(x: number, y: number): T  {
+        if (x < 0 || x >= this._width) {
+            throw new Error('x is out of range');
+        }
+        if (y < 0 || y >= this._height) {
+            throw new Error('y is out of range');
+        }
+        return this._items[this.calculateIndex(x, y)];
+    }
+
+    private unsageGetItemAt(x: number, y: number): T | undefined {
+        if (x < 0 || x >= this._width || y < 0 || y >= this._height) {
+            return undefined;
+        }
+        return this._items[this.calculateIndex(x, y)];
+    }
+
+    public setItemAt(x: number, y: number, value: T) {
+        if (x < 0 || x >= this._width || y < 0 || y >= this._height) {
+            return;
+        }
+
+        this._items[this.calculateIndex(x, y)] = value;
+    }
+
+    public fill(generator: (x: number, y: number) => T): void {
+        this.iterate((_, x, y) => this.setItemAt(x, y, generator(x, y)));
+    }
+
+    public iterate(callback: (item: T, x: number, y: number) => void): void {
+        for(let x = 0; x < this._height; x++) {
+            for(let y = 0; y < this._width; y++) {
+                callback(this.itemAt(x, y), x, y);
+            }
+        }
+    }
+
+    public getItemsAround(x: number, y: number): T[] {
+        const result = [];
+        for (let i = -1; i <= 1; i++) {
+            for(let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) {
+                    continue;
+                }
+                result.push(this.unsageGetItemAt(x+i, y+j));
+            }
+        }
+
+        return result.filter(item => item !== undefined) as T[];
+    }
+
+    private calculateIndex(x: number, y: number): number {
+        return x*this._width + y;
+    }
+}
